837571c1236aa9ca29ef2022016b240a3fd72319
13ulbasaur <13ulbasaur@DESKTOP-HOME>
2022-10-08 12:27:22 +1300

    fix url

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#btnRun").click(function () {
-        $.get('https://randomzack.twiggies.workers.dev/',
+        $.get('https://flightrising.com/includes/ol/scryer_bloodlines.php?id1=1&id2=2',
             function (data) {  // success callback
                 var result = data.getElementsByTagName('div')[1];
                 console.log(result);
