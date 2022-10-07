95f4013056732a7eaf9cc88faad89dde7dcaaced
13ulbasaur <13ulbasaur@DESKTOP-HOME>
2022-10-08 12:12:20 +1300

    Remove apple icon and this.hide .

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $("#btnRun").click(function(){
-      $(this).hide();
       $.ajax({
-          url: "https://randomzack.twiggies.workers.dev/", success: function (result) {
+          type: "GET",
+          url: "https://randomzack.twiggies.workers.dev/",
+          success: function (result) {
               $("#zackTime").src(result);
           }
       });
