56b2a347e1303b0a83bed9a24d72f098f765c66d
13ulbasaur <115045104+13ulbasaur@users.noreply.github.com>
2022-10-08 13:30:13 +1300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,9 @@
 $(document).ready(function () {
     $("#btnRun").click(function () {
-$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://google.com',) + '&callback=?', function(data){
-	alert(data.contents);
-});
-$.ajax({
-  url: 'https://flightrising.com/includes/ol/scryer_bloodlines.php?id1=1&id2=2', ,
-  dataType: "jsonp",
-  success: function (data) {
-    console.log(data)
-    alert(data);
-  }
-});
-        $.get('https://flightrising.com/includes/ol/scryer_bloodlines.php?id1=1&id2=2',
+//$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://google.com',) + '&callback=?', function(data){
+	//alert(data.contents);
+//});
+        $.get('https://flightrising.com/main.php?p=scrying&view=bloodlines',
             function (data) {  // success callback
                 var result = data.getElementsByTagName('div')[1];
                 console.log(result);
